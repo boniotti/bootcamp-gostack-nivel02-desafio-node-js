d8e83b3700360799542a5c8e66d754e7ccfb028c
Fernando Boniotti <fernando.boniotti@gmail.com>
2020-04-14 10:36:12 -0300

    Entrega do desafio Bootcamp GoStack

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,38 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (total: any, balance) => {
+        const key = balance.type;
+        if (!total[key]) {
+          total[key] = 0;
+        }
+        total[key] += balance.value;
+
+        return total;
+      },
+      {},
+    );
+
+    const total = (income || 0) - (outcome || 0);
+
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
